641f0461bd17b34f07254914055d616980bb0c24
Uncle Bamdad <mohammadJavadTabari1024@outlook.com>
2025-05-05 23:39:57 -0700

    city extension

---
+++ Datalayer/EntityFramework/Extensions/CityExtension.cs
@@ -0,0 +1,28 @@
+﻿using DataLayer.Assistant.Enums;
+
+namespace DataLayer;
+public static class CityExtension
+{
+	public static IQueryable<City> ApplyFilter(this IQueryable<City> query, DefaultPaginationFilter filter)
+	{
+
+		if (!string.IsNullOrEmpty(filter.Keyword))
+			query = query.Where(x => x.name.ToLower().Contains(filter.Keyword.ToLower().Trim())
+			|| x.slug.ToLower().Contains(filter.Keyword.ToLower().Trim()));
+
+		return query;
+	}
+
+
+	public static IQueryable<City> ApplySort(this IQueryable<City> query, SortByEnum? sortBy)
+	{
+		return sortBy switch
+		{
+			SortByEnum.CreationDate => query.OrderBy(x => x.created_at),
+			SortByEnum.CreationDateDescending => query.OrderByDescending(x => x.created_at),
+			SortByEnum.updated_ate => query.OrderBy(x => x.updated_at),
+			SortByEnum.updated_ateDescending => query.OrderByDescending(x => x.updated_at),
+			_ => query.OrderByDescending(x => x.id)
+		};
+	}
+}

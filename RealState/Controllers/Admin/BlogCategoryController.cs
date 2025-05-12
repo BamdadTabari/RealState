0a75255466801b92e2b51a23b4765191016a6ca4
Uncle Bamdad <mohammadJavadTabari1024@outlook.com>
2025-05-12 01:30:25 -0700

    fixed

--- RealState/Controllers/Admin/BlogCategoryController.cs
+++ RealState/Controllers/Admin/BlogCategoryController.cs
@@ -181,7 +181,7 @@ public class BlogCategoryController(IUnitOfWork unitOfWork) : ControllerBase
 
     [HttpPost]
     [Route("delete")]
-    public async Task<IActionResult> Delete([FromRoute] long id)
+    public async Task<IActionResult> Delete([FromForm] long id)
     {
         var entity = await _unitOfWork.BlogCategoryRepository.Get(id);
         if (entity == null)

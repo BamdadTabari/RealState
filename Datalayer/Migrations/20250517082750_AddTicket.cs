6c364d22c14c6493f80c0203b8011957ddfc3068
Uncle Bamdad <mohammadJavadTabari1024@outlook.com>
2025-05-17 01:34:30 -0700

    add migration

---
+++ Datalayer/Migrations/20250517082750_AddTicket.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTicket : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contact");
+
+            migrationBuilder.CreateTable(
+                name: "Ticket",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ticket_code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    subject = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    picture = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    status = table.Column<int>(type: "int", nullable: false),
+                    user_id = table.Column<long>(type: "bigint", nullable: false),
+                    is_admin = table.Column<bool>(type: "bit", nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    slug = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ticket", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Ticket_User_user_id",
+                        column: x => x.user_id,
+                        principalTable: "User",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TicketReply",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    picture = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ticket_id = table.Column<long>(type: "bigint", nullable: false),
+                    user_id = table.Column<long>(type: "bigint", nullable: false),
+                    is_admin = table.Column<bool>(type: "bit", nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    slug = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TicketReply", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_TicketReply_Ticket_ticket_id",
+                        column: x => x.ticket_id,
+                        principalTable: "Ticket",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TicketReply_User_user_id",
+                        column: x => x.user_id,
+                        principalTable: "User",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ticket_slug",
+                table: "Ticket",
+                column: "slug",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ticket_user_id",
+                table: "Ticket",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TicketReply_slug",
+                table: "TicketReply",
+                column: "slug",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TicketReply_ticket_id",
+                table: "TicketReply",
+                column: "ticket_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TicketReply_user_id",
+                table: "TicketReply",
+                column: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TicketReply");
+
+            migrationBuilder.DropTable(
+                name: "Ticket");
+
+            migrationBuilder.CreateTable(
+                name: "Contact",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    created_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    full_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    slug = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    updated_at = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contact", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contact_slug",
+                table: "Contact",
+                column: "slug",
+                unique: true);
+        }
+    }
+}

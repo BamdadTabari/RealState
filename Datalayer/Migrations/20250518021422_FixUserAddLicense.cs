5e799904a70e6dba3a7e26c921caf5b22aa3fafc
Uncle Bamdad <mohammadJavadTabari1024@outlook.com>
2025-05-17 19:15:00 -0700

    Add migration and update database

---
+++ Datalayer/Migrations/20250518021422_FixUserAddLicense.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixUserAddLicense : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_agency",
+                table: "User",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_licensed",
+                table: "User",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "license",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "id",
+                keyValue: 1L,
+                columns: new[] { "is_agency", "is_licensed", "license" },
+                values: new object[] { false, false, null });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_agency",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "is_licensed",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "license",
+                table: "User");
+        }
+    }
+}

51703983a459b8837599099a333794d52bec0837
Uncle Bamdad <mohammadJavadTabari1024@outlook.com>
2025-05-17 21:58:21 -0700

    home index

---
+++ RealState/Controllers/Admin/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using DataLayer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RealState.Models;
+
+namespace RealState.Controllers.Admin;
+[Route("api/home")]
+[ApiController]
+[Authorize(Roles = "Admin,MainAdmin")]
+public class HomeController(IUnitOfWork unitOfWork) : ControllerBase
+{
+	private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+	[HttpGet]
+	[Route("index")]
+	public IActionResult Index()
+	{
+		var userCount = _unitOfWork.UserRepository.Count();
+		var propertyCount = _unitOfWork.PropertyRepository.Count();
+		var ticketCount = _unitOfWork.TicketRepository.Count();
+		var pendingPropertiesCount = _unitOfWork.PropertyRepository.Count(x=>x.state_enum == AdStatusEnum.NotConfirmed);
+
+		return Ok(new ResponseDto<DashboardDto>()
+		{
+			data = new DashboardDto()
+			{
+				users = userCount,
+				pending_properties = pendingPropertiesCount,
+				properties = propertyCount,
+				tickets = ticketCount
+			},
+			is_success = true,
+			message = "",
+			response_code = 200
+		});
+	}
+}
---
+++ RealState/Models/DashboardDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RealState.Models;
+
+public class DashboardDto
+{
+	public int users { get; set; }
+	public int properties { get; set; }
+	public int tickets { get; set; }
+	public int pending_properties { get; set; }
+}

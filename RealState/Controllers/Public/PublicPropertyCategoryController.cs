43bff439dd9d5992b441d17d396cd7a3a873fd2d
Uncle Bamdad <mohammadJavadTabari1024@outlook.com>
2025-05-28 10:52:39 -0700

    prop category

---
+++ RealState/Controllers/Public/PublicPropertyCategoryController.cs
@@ -0,0 +1,27 @@
+﻿using DataLayer;
+using DataLayer.Assistant.Enums;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealState.Models;
+
+namespace RealState.Controllers.Public;
+[Route("api/public/property-category")]
+[ApiController]
+public class PublicPropertyCategoryController(IUnitOfWork unitOfWork) : ControllerBase
+{
+	private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+	[HttpGet]
+	[Route("all")]
+	public async Task<IActionResult> Index()
+	{
+		var data = await _unitOfWork.PropertyCategoryRepository.GetAll();
+		return Ok(new ResponseDto<List<PropertyCategory>>()
+		{
+			data = data,
+			is_success = true,
+			message = "",
+			response_code = 200
+		});
+	}
+}
